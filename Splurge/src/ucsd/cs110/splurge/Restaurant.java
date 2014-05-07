9891214d0105f9cf5ac759b1d651755dd47a599f
Thomas Tucker <elteano@gmail.com>
2014-05-07 11:00:13 -0700

    Added commenting to Restaurant.java

--- Splurge/src/ucsd/cs110/splurge/Restaurant.java
+++ Splurge/src/ucsd/cs110/splurge/Restaurant.java
@@ -4,13 +4,42 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 
+/**
+ * Object containing all information specific to an individual restaurant.
+ */
 public class Restaurant {
 
+	/**
+	 * Menus available for the restaurant. These are labeled individually.
+	 */
 	private Collection<FoodMenu> mMenus;
+	/**
+	 * Times which are currently unavailable for reservation.
+	 *
+	 * It is expected that the default iteration through this Collection
+	 * provides unavailable times in sorted order.
+	 */
 	private Collection<Timeslot> mUnavailableTimes;
+	/**
+	 * Hours during which this restaurant is open for business, indexed by day
+	 * starting at Monday.
+	 */
 	private Timeslot[] mOpenHours;
+	/**
+	 * Name of the restaurant.
+	 */
 	private String mRestaurantName;
 
+	/**
+	 * Creates a new restaurant with nothing but a name.
+	 *
+	 * Container objects are initialized, but not populated. Population is to
+	 * occur elsewhere, and it is the responsibility of the owning object to
+	 * undertake.
+	 *
+	 * @param name
+	 *            The name of the Restaurant.
+	 */
 	public Restaurant(String name) {
 		mRestaurantName = name;
 		mMenus = new ArrayList<FoodMenu>();
@@ -23,11 +52,25 @@ public class Restaurant {
 					Calendar.getInstance());
 		}
 	}
-	
+
+	/**
+	 * Retrieves the name of the restaurant in user-friendly form.
+	 *
+	 * @return A printable name of the restaurant.
+	 */
 	public String getName() {
 		return mRestaurantName;
 	}
 
+	/**
+	 * Checks through the unavailable timeslots to determine if the given
+	 * instant of time is unavailable.
+	 *
+	 * @param time
+	 *            The instant in time to check for unavailability.
+	 * @return <code>true</code> if the time is within reserved space.
+	 * @see isTimeUnavailalble(Calendar)
+	 */
 	private boolean isTimeWithinReservation(Calendar time) {
 		for (Timeslot t : mUnavailableTimes) {
 			if (t.withinTimeslot(time))
@@ -36,10 +79,26 @@ public class Restaurant {
 		return false;
 	}
 
+	/**
+	 * Determines whether the given time is taken.
+	 *
+	 * @param time
+	 *            The point in time to check.
+	 * @return <code>true</code> if the time is unavailable, <code>false</code>
+	 *         otherwise.
+	 */
 	public boolean isTimeUnavailable(Calendar time) {
 		return isTimeWithinReservation(time);
 	}
 
+	/**
+	 * Retrieves a menu by its title. If no such menu exist, this function will
+	 * return <code>null</code>.
+	 *
+	 * @param label
+	 *            The title of the menu.
+	 * @return The menu, or <code>null</code> if no such menu exists.
+	 */
 	public FoodMenu getMenuByLabel(String label) {
 		for (FoodMenu m : mMenus) {
 			if (m.getMenuName().equals(label))
@@ -48,6 +107,15 @@ public class Restaurant {
 		return null;
 	}
 
+	/**
+	 * Gets the hours available for a given day. If hours are unavailable on
+	 * that day, then the Timeslot value will be <code>null</code>.
+	 *
+	 * @param day
+	 *            Enum value depicting which day to check.
+	 * @return The Timeslot depicting hours for the given day, or
+	 *         <code>null</code> if closed all day.
+	 */
 	public Timeslot getHoursForDay(DayOfWeek day) {
 		switch (day) {
 		case MONDAY:
