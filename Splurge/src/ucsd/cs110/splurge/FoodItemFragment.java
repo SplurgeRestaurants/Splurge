708e8d3bf00d81b4dceb7041b8718f8b29bf75d3
dqthai <dthai1994@gmail.com>
2014-05-11 22:32:23 -0700

    -added chao's xml file

--- Splurge/src/ucsd/cs110/splurge/FoodItemFragment.java
+++ Splurge/src/ucsd/cs110/splurge/FoodItemFragment.java
@@ -9,11 +9,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class FoodItemFragment extends Fragment{
-	//View Items in fragment
-	TextView name;				//food item name
-	TextView description;		//food item description
-	ImageView image;			//food item image
+public class FoodItemFragment extends Fragment {
+	// View Items in fragment
+	TextView name; // food item name
+	TextView description; // food item description
+	TextView price; // food item price
+	ImageView image; // food item image
+
 	@Override
 	/**
 	 * Get information for the view
@@ -21,20 +23,25 @@ public class FoodItemFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//Get the layout
+		// Get the layout
 		View ret = inflater.inflate(R.layout.food_item, container, false);
 		// get view ids
 		name = (TextView) ret.findViewById(R.id.food_name);
 		description = (TextView) ret.findViewById(R.id.food_description);
 		image = (ImageView) ret.findViewById(R.id.food_image);
-		//get the intent to find the position of the food item in the food menu
+		price = (TextView) ret.findViewById(R.id.food_price);
+		// get the intent to find the position of the food item in the food menu
 		Intent intent = getActivity().getIntent();
 		int position = 0;
-		position = intent.getIntExtra(FoodMenuActivity.FOOD_ITEM_POSITION, position);
+		position = intent.getIntExtra(FoodMenuActivity.FOOD_ITEM_POSITION,
+				position);
 		// set the information to the appropriate views
 		name.setText(FoodMenuActivity.data.get(position).getName());
 		image.setImageResource(FoodMenuActivity.data.get(position).getImage());
 		description.setText(FoodMenuActivity.data.get(position).getName());
+		price.setText(Integer.toString(FoodMenuActivity.data.get(position)
+				.getPrice()));
+
 		return ret;
 	}
 }
