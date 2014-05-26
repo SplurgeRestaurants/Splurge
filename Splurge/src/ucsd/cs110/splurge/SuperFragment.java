7b73305961b572547f97f9f6751f5a99c55b1ca5
Thomas Tucker <elteano@gmail.com>
2014-05-26 16:32:42 -0700

    Added comments.

--- Splurge/src/ucsd/cs110/splurge/SuperFragment.java
+++ Splurge/src/ucsd/cs110/splurge/SuperFragment.java
@@ -2,9 +2,26 @@ package ucsd.cs110.splurge;
 
 import android.app.Fragment;
 
+/**
+ * Fragment superclass which details the requirements for all fragments used in
+ * the application so as to conform to the implementation of the MVC framework.
+ */
 public abstract class SuperFragment extends Fragment {
 	protected SuperListener mSuperListener;
 
+	/**
+	 * Sets the listener class for the Fragment.
+	 * 
+	 * This does not hook up the listener to any particular function, it merely
+	 * registers the listener to the fragment so that the fragment can hook up
+	 * the listener as need be. This hookup should occur in the
+	 * <code>onStart</code> or <code>onResume</code> of the Fragment so as to
+	 * avoid attempting to hook up the listener prior to there being an object
+	 * to which to listen.
+	 * 
+	 * @param l
+	 *            The Fragment's new listener.
+	 */
 	public void setSuperListener(SuperListener l) {
 		mSuperListener = l;
 	}
