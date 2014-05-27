1a69008f44690c28e7e985b04a6f097317cfb0eb
Thomas Tucker <elteano@gmail.com>
2014-05-26 21:08:21 -0700

    Fixed log message

--- Splurge/src/ucsd/cs110/splurge/RestaurantListFragment.java
+++ Splurge/src/ucsd/cs110/splurge/RestaurantListFragment.java
@@ -8,6 +8,7 @@ import ucsd.cs110.splurge.model.Restaurant;
 import ucsd.cs110.splurge.model.RestaurantListing;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
