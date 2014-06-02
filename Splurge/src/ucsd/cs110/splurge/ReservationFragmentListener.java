4b810e3dbcc39eb6be5c25aceed4a06618e70306
dqthai <dthai1994@gmail.com>
2014-06-01 18:48:07 -0700

    got rid of reservation confirmation, made dialog instead

--- Splurge/src/ucsd/cs110/splurge/ReservationFragmentListener.java
+++ Splurge/src/ucsd/cs110/splurge/ReservationFragmentListener.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.NumberPicker;
 import android.widget.NumberPicker.OnScrollListener;
+import android.widget.Toast;
 
 /**
  * Listener for the RestaurantMainMenuFragment
@@ -12,6 +13,8 @@ import android.widget.NumberPicker.OnScrollListener;
  */
 public class ReservationFragmentListener extends SuperListener implements
 		OnClickListener, OnScrollListener {
+
+	private boolean reservationSuccessful = false;
 
 	/**
 	 * Create a new ReservationFragmentListener, designed to listen to a
@@ -44,7 +47,14 @@ public class ReservationFragmentListener extends SuperListener implements
 			Log.e("Splurge", selectedHour + " " + selectedMinute + " "
 					+ selectedAmPm);
 			Log.e("Splurge", "Party size is " + getPartySize);
-			mWrapper.changeFragment(new ReservationConfirmation(), null);
+			if (reservationSuccessful) {
+				Toast.makeText(mWrapper, "Reservation Successful",
+						Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(mWrapper,
+						"Reservation Unsuccessful, pick another time",
+						Toast.LENGTH_LONG).show();
+			}
 			break;
 		default:
 			break;
