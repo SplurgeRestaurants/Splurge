64101a7046ce952799971884eca1498bfb4d455b
Thomas Tucker <elteano@gmail.com>
2014-06-02 16:22:41 -0700

    Interpreter for restaurant information now more lenient about missing fields.

--- Splurge/src/ucsd/cs110/splurge/connectivity/RestaurantInfoResponseMessage.java
+++ Splurge/src/ucsd/cs110/splurge/connectivity/RestaurantInfoResponseMessage.java
@@ -14,6 +14,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PointF;
 import android.util.Base64;
+import android.util.Log;
 
 /**
  * Class for handling received JSON queries in response to a restaurant
@@ -50,46 +51,58 @@ public class RestaurantInfoResponseMessage {
 			String name = inputJSON.getString("restaurantName");
 			Restaurant encaps = new Restaurant(name);
 
-			encaps.setId(inputJSON.getInt("restaurantID"));
+			encaps.setId(inputJSON.getInt("restaurantId"));
 
-			JSONArray locArray = inputJSON.getJSONArray("restaurantLocation");
-			encaps.setLocation(new PointF((float) locArray.getDouble(0),
-					(float) locArray.getDouble(1)));
+			if (inputJSON.has("restaurantLocation")) {
+				JSONArray locArray = inputJSON
+						.getJSONArray("restaurantLocation");
+				encaps.setLocation(new PointF((float) locArray.getDouble(0),
+						(float) locArray.getDouble(1)));
+			}
 
 			// Add unavailable times
-			JSONArray unavailables = inputJSON
-					.getJSONArray("restaurantAvailability");
-			for (int i = 0; i < unavailables.length(); ++i) {
-				JSONObject obj = unavailables.getJSONObject(i);
-				Calendar start, end;
-				start = Calendar.getInstance();
-				end = Calendar.getInstance();
-				start.setTimeInMillis(obj.getLong("start"));
-				end.setTimeInMillis(obj.getLong("end"));
-				encaps.addUnavailableTime(new Timeslot(start, end));
+			if (inputJSON.has("restaurantAvailability")) {
+				JSONArray unavailables = inputJSON
+						.getJSONArray("restaurantAvailability");
+				for (int i = 0; i < unavailables.length(); ++i) {
+					JSONObject obj = unavailables.getJSONObject(i);
+					Calendar start, end;
+					start = Calendar.getInstance();
+					end = Calendar.getInstance();
+					start.setTimeInMillis(obj.getLong("start"));
+					end.setTimeInMillis(obj.getLong("end"));
+					encaps.addUnavailableTime(new Timeslot(start, end));
+				}
 			}
 
 			// Extract menus
-			JSONArray menuArray = inputJSON.getJSONArray("restaurantMenus");
-			for (int menuInd = 0; menuInd < menuArray.length(); ++menuInd) {
-				JSONObject currentMenu = menuArray.getJSONObject(menuInd);
-				FoodMenu buildMenu = new FoodMenu(
-						currentMenu.getString("title"));
-				JSONArray menuItems = currentMenu.getJSONArray("items");
-				for (int itemInd = 0; itemInd < currentMenu.length(); ++itemInd) {
-					JSONObject foodJSON = menuItems.getJSONObject(itemInd);
-					FoodItem food = new FoodItem(foodJSON.getString("itemName"));
-					byte[] decodedImage = Base64.decode(
-							foodJSON.getString("itemImage"), Base64.DEFAULT);
-					Bitmap image = BitmapFactory.decodeByteArray(decodedImage,
-							0, decodedImage.length);
-					food.setImage(image);
-					buildMenu.addFoodItem(food);
+			if (inputJSON.has("restaurantMenus")) {
+				JSONArray menuArray = inputJSON.getJSONArray("restaurantMenus");
+				for (int menuInd = 0; menuInd < menuArray.length(); ++menuInd) {
+					JSONObject currentMenu = menuArray.getJSONObject(menuInd);
+					FoodMenu buildMenu = new FoodMenu(
+							currentMenu.getString("title"));
+					JSONArray menuItems = currentMenu.getJSONArray("items");
+					for (int itemInd = 0; itemInd < currentMenu.length(); ++itemInd) {
+						JSONObject foodJSON = menuItems.getJSONObject(itemInd);
+						FoodItem food = new FoodItem(
+								foodJSON.getString("itemName"));
+						byte[] decodedImage = Base64
+								.decode(foodJSON.getString("itemImage"),
+										Base64.DEFAULT);
+						Bitmap image = BitmapFactory.decodeByteArray(
+								decodedImage, 0, decodedImage.length);
+						food.setImage(image);
+						buildMenu.addFoodItem(food);
+					}
 				}
 			}
 
 			return new RestaurantInfoResponseMessage(encaps);
 		} catch (JSONException e) {
+			Log.i("Splurge",
+					"Unexpected JSON Error: " + e.getLocalizedMessage());
+			Log.i("Splurge", e.getMessage());
 			return null;
 		}
 	}
