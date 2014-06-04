69d38f6008cb13110686a2ef68e7fd9e0831a783
dqthai <dthai1994@gmail.com>
2014-06-04 12:59:57 -0700

    calendar fixes

--- Splurge/src/ucsd/cs110/splurge/CalendarAdapter.java
+++ Splurge/src/ucsd/cs110/splurge/CalendarAdapter.java
@@ -86,32 +86,6 @@ public class CalendarAdapter extends BaseAdapter {
 			}
 		}
 		dayView.setText(days[position]);
-		int dayOfWeek = selectedDate.get(Calendar.DAY_OF_WEEK);
-		String day = "";
-		switch (dayOfWeek) {
-		case Calendar.SUNDAY:
-			day = "Sunday";
-			break;
-		case Calendar.MONDAY:
-			day = "Monday";
-			break;
-		case Calendar.TUESDAY:
-			day = "Tuesday";
-			break;
-		case Calendar.WEDNESDAY:
-			day = "Wednesday";
-			break;
-		case Calendar.THURSDAY:
-			day = "Thursday";
-			break;
-		case Calendar.FRIDAY:
-			day = "Friday";
-			break;
-		case Calendar.SATURDAY:
-			day = "Saturday";
-			break;
-		}
-		dayOfTheWeek.setText(day);
 		// create date string for comparison
 		String date = days[position];
 
