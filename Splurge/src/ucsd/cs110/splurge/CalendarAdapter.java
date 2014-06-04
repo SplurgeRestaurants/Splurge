5c0c5373731ca398733bede5e62a00ca1f7dfd3f
dqthai <dthai1994@gmail.com>
2014-06-03 20:38:20 -0700

    display day of the week in calendar

--- Splurge/src/ucsd/cs110/splurge/CalendarAdapter.java
+++ Splurge/src/ucsd/cs110/splurge/CalendarAdapter.java
@@ -67,6 +67,7 @@ public class CalendarAdapter extends BaseAdapter {
 		}
 
 		dayView = (TextView) v.findViewById(R.id.date);
+		TextView dayOfTheWeek = (TextView) v.findViewById(R.id.day_of_the_week);
 
 		// disable empty days from the beginning
 		if (days[position].equals("")) {
@@ -85,7 +86,32 @@ public class CalendarAdapter extends BaseAdapter {
 			}
 		}
 		dayView.setText(days[position]);
-
+		int dayOfWeek = selectedDate.get(Calendar.DAY_OF_WEEK);
+		String day = "";
+		switch (dayOfWeek) {
+		case Calendar.SUNDAY:
+			day = "Sunday";
+			break;
+		case Calendar.MONDAY:
+			day = "Monday";
+			break;
+		case Calendar.TUESDAY:
+			day = "Tuesday";
+			break;
+		case Calendar.WEDNESDAY:
+			day = "Wednesday";
+			break;
+		case Calendar.THURSDAY:
+			day = "Thursday";
+			break;
+		case Calendar.FRIDAY:
+			day = "Friday";
+			break;
+		case Calendar.SATURDAY:
+			day = "Saturday";
+			break;
+		}
+		dayOfTheWeek.setText(day);
 		// create date string for comparison
 		String date = days[position];
 
