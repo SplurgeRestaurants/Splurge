83a95ac95dc1e1d67e05f3abfbd82d09ba81713a
Thomas Tucker <elteano@gmail.com>
2014-06-04 17:02:15 -0700

    Fixed bug where restaurant list would not asynchronously display selected restaurant.

---
+++ Splurge/src/ucsd/cs110/splurge/model/NullOnRestaurantReadyListener.java
@@ -0,0 +1,12 @@
+package ucsd.cs110.splurge.model;
+
+/**
+ * Null implementation of the OnRestaurantReadyListener.
+ */
+public class NullOnRestaurantReadyListener implements OnRestaurantReadyListener {
+
+	@Override
+	public void onRestaurantReady() {
+		// Do nothing
+	}
+}
